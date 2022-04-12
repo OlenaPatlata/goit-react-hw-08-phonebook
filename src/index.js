fa75e41a7b2fb92618244566cb2746324aaa2e79
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-04-12 17:05:55 +0300

    started1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-{
-  /* ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-); */
-}
